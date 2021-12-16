e3506a798dd6e7588d5d9bdf99c7ab04cba24519
Alberto Jose <albertojcvs@gmail.com>
2021-12-16 09:46:48 -0300

    add the kafka consumer in the serer file

--- Save_service/src/server.ts
+++ Save_service/src/server.ts
@@ -1,30 +1,13 @@
-import express from "express";
+import { PrismaClient } from ".prisma/client";
+import express, { response } from "express";
+import { Consumer } from "./kafkaServices/Consumer";
 import { Kafka } from "kafkajs";
+import { PrismaUserRepository } from "./repositories/implementaions/PrismaUserRepository";
 const app = express();
 
-(async () => {
-  const kafka = new Kafka({
-    clientId: "my-app",
-    brokers: ["kafka_kafka_1:29092"],
-  });
+const consumer = new Consumer('save_service_1')
 
-  const consumer = kafka.consumer({ groupId: "test-group" });
-
-  await consumer.connect();
-  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log('aaaaaaaaaaaaaaaa', {
-        value: message.value?.toString(),
-      });
-    },
-  });
-})();
-app.get("/", (request, response) => {
-  response.send("Serve Two");
-});
-
+consumer.consume()
 app.listen(3334, () => {
   console.log("Serve two is running");
 });
